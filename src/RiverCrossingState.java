eeda153cedac41274a9dd25fd22e82a08c28933e
rjesteban <ajcp.esteban@gmail.com>
2016-03-08 22:28:40 +0800

    added comments

--- src/RiverCrossingState.java
+++ src/RiverCrossingState.java
@@ -7,17 +7,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
- * weirdest implementation ever. will do better oop looking code next time.
+ * weirdest implementation ever. All if else
+ * will do better oop looking code next time.
+ * I hereby solemnly swear that I did code this by myself.
  */
 /**
  *
- * @author rj
+ * @author 2013-50109 Esteban, Arnold Joseph Caesar P.
+ * @version 1.0
+ * @since 2016-3-8
  */
 public class RiverCrossingState {
 
     private ArrayList<Character> west;
     private ArrayList<Character> east;
-    private boolean isBbCSWest;
+    private boolean isBbCSWest; //checks whether bbComsci is in the west or east
 
     public RiverCrossingState(ArrayList<Character> west, ArrayList<Character> east) {
         this.west = west;
@@ -25,16 +29,25 @@ public class RiverCrossingState {
         isBbCSWest = true;
     }
   
+    /**
+     * reset sets the state back to the initial state
+    */
     public void reset(){
         isBbCSWest = true;
         this.west.clear();
         this.east.clear();
-        west.add('L');
-        west.add('R');
-        west.add('C');
+        west.add('L'); //Lion
+        west.add('R'); //Rabbit
+        west.add('C'); //Carrot
         isBbCSWest = true;
     }
-
+    
+    /**
+     * isIllegalState checks whether one of Bb Comsci's pets will eat one of
+     * his possessions. This happens when he leaves them alone. Certain 
+     * conditions are followed (based on the problem)
+     * @return true if illegal; false otherwise
+     */
     public boolean isIllegalState() {
         if (west.contains('L') && west.contains('R') && west.contains('C') && isBbCSWest == false) {
             return true;
@@ -52,25 +65,43 @@ public class RiverCrossingState {
         return false;
     }
 
+    /**
+     * isEndingState checks whether Bb Comsci was able to cross all of his
+     * possessions to the east bank
+     * @return true if he was able to; false otherwise
+     */
     public boolean isEndingState() {
-        return (east.contains('L') && east.contains('R') && east.contains('C') && isBbCSWest == false);
+        return (east.contains('L') && east.contains('R') && east.contains('C') 
+                && isBbCSWest == false);
     }
     
+    
+    /**
+     * toString returns the current state. "_" separates the characters
+     * that are in the west bank and east bank.
+     * @return state, the current state of Bb Comsci's river cross challenge
+     */
+    @Override
     public String toString(){
-        String ret = "";
+        String state = "";
         for (char c: west)
-            ret += c;
+            state += c;
         if (isBbCSWest == true)
-            ret += 'N';
-        ret += "_";
+            state += 'N';
+        state += "_";
         for (char c: east)
-            ret += c;
+            state += c;
         if (isBbCSWest == false)
-            ret += 'N';
+            state += 'N';
         
-        return ret;
+        return state;
     }
-
+    
+    /**
+     * MOVE: when bbCS travels to the other bank, with or without his possession
+     * @param c movement based on NLRC
+     * @return true if movement is allowed; false otherwise
+     */
     public boolean move(char c) {
         if (c == 'N') {
             isBbCSWest = !isBbCSWest;
@@ -116,6 +147,12 @@ public class RiverCrossingState {
         return false;
     }
 
+    /**
+     * solve: check the movements (NLRC) whether it passes the "dfa" or not
+     * @param fileName the inputs, delimited by line
+     * @throws FileNotFoundException Scanner parameter
+     * @throws IOException FileWriter.
+     */    
     public void solve(String fileName) throws FileNotFoundException, IOException {
         Scanner sc = new Scanner(new File(fileName));
         FileWriter f = new FileWriter(fileName.split("[.]")[0]+".out");
@@ -124,24 +161,27 @@ public class RiverCrossingState {
             for (int i = 0; i < s.length(); i++) {
                 char move = s.charAt(i);
                 if (!this.move(move)) {
-                    //System.out.println("NG");
                     f.append("NG\n");
                     break;
                 }
                 if (this.isIllegalState()) {
-                    //System.out.println("NG");
                     f.append("NG\n");
                     break;
                 }
             }
             if (this.isEndingState())
                 f.append("OK\n");
-                //System.out.println("OK");
             reset();
         }
         f.close();
     }
-    
+
+    /**
+     * solve: check the movements (NLRC) whether it passes the "dfa" or not
+     * @param args args[0] filename for input
+     * @throws FileNotFoundException Scanner parameter (solve)
+     * @throws IOException FileWriter (solve)
+     */    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         ArrayList<Character> west, east;
         west = new ArrayList<Character>();
